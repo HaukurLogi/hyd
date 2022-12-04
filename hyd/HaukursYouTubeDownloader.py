c40e8e179b23e2508a7cbe7b0cb96c74672ed863
haukurlogi <haukurlogi2008@gmail.com>
2022-12-04 09:10:29 +0000

    Fixed color in popup

--- hyd/HaukursYouTubeDownloader.py
+++ hyd/HaukursYouTubeDownloader.py
@@ -8,15 +8,15 @@ def main():
     window = Tk()
     path = os.path.join(os.path.join(os.environ["HOME"]), 'YoutubeDownloads') # Make path
     os.makedirs(path, exist_ok=True) # Create folder if it doesn't exist already
-    start_time = 0 # Start time
-    end_time = 0 # Stop time
+    start_time = 0 
+    end_time = 0
     mp3IsClicked = False
 
     # The window itself
     window.geometry('600x150')
     window.resizable(False, False)
     window.configure(bg="#222222")
-    window.title('''Haukur's YouTube Downloader''') # Title in the fixedtitle bar
+    window.title('''Haukur's YouTube Downloader''')
 
     # Paste label
     link = StringVar()
@@ -63,32 +63,32 @@ def main():
     downloadButtonMP4.place(relx=0.37, rely=0.7, anchor=CENTER)
     downloadButtonMP3.place(relx=0.63, rely=0.7, anchor=CENTER)
 
-
     def mp3IsClickedFunction():
         global mp3IsClicked
         mp3IsClicked = True
         download(link.get())
 
     def download(link):
+        global mp3IsClicked
         try:
             fixedtitle = YouTube(link).title
-            unwanted_chars = ".$'%"
+            unwanted_chars = ".$'%-"
             for char in unwanted_chars:
                 fixedtitle = fixedtitle.replace(char, "")
             start_time = time()
             YouTube(link).streams.get_highest_resolution().download(path)
-            if os.path.exists(f"{path}/{fixedtitle}.mp4") and mp3IsClicked == True:
-                video = VideoFileClip(f"{path}/{fixedtitle}.mp4")
-                os.remove(f"{path}/{fixedtitle}.mp4")
-                video.audio.write_audiofile(f"{path}/{fixedtitle}.mp3")
+            if os.path.exists(f"{path}/{fixedtitle}.mp4"):
+                if mp3IsClicked == True:
+                    video = VideoFileClip(f"{path}/{fixedtitle}.mp4")
+                    os.remove(f"{path}/{fixedtitle}.mp4")
+                    video.audio.write_audiofile(f"{path}/{fixedtitle}.mp3")
             end_time = time()
-            mp3IsClicked == False
             popup = Tk()
             popup.title("Download Success!")
             popup.geometry("350x125")
             popup.resizable(False, False)
-            popup.configure(bg="#333333")
-            popupLabel = Label(popup, text=f"Successfuly downloaded video at \n{path}.\nTotal time taken: {round(end_time-start_time,3)} seconds", fg="#EDEDED", bg="#333333")
+            popup.configure(bg="#222222")
+            popupLabel = Label(popup, text=f"Successfuly downloaded video at \n{path}.\nTotal time taken: {round(end_time-start_time,3)} seconds", fg="#EDEDED", bg="#222222")
             popupLabel.place(relx=0.5, rely=0.25, anchor=CENTER)
             popup.mainloop()
         except:
@@ -96,11 +96,12 @@ def main():
             error.title("Download Failed :/")
             error.geometry("350x125")
             error.resizable(False, False)
-            error.configure(bg="#333333")
-            errorLabel = Label(error, text="Download Failed :/ \n Please check your internet connection.",  fg="#EDEDED", bg="#333333")
+            error.configure(bg="#222222")
+            errorLabel = Label(error, text="Download Failed :/ \n Please check your internet connection.",  fg="#EDEDED", bg="#222222")
             errorLabel.place(relx=0.5, rely=0.25, anchor=CENTER)
             error.mainloop()
 
     window.mainloop()
 if __name__ == '__main__':
-  main()
+    main()
+ 
